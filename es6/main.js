42aefe1624fd3faa2bb89e923bc07b4382083364
Stephen Matta <stephenjmatta+github@gmail.com>
2015-10-27 11:47:03 -0400

    3000 markers demo

--- es6/main.js
+++ es6/main.js
@@ -12,18 +12,28 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 
 L.Icon.Default.imagePath = 'http://api.tiles.mapbox.com/mapbox.js/v1.0.0beta0.0/images';
 
-const data = _.times(1000, () => {
-  return [faker.address.latitude(), faker.address.longitude()];
+const data = _.times(3000, (i) => {
+  return {
+    id: i,
+    latLng: [faker.address.latitude(), faker.address.longitude()],
+  };
 });
 
 const d3Overlay = L.d3SvgOverlay((selection, projection) => {
-  const updateSelection = selection.selectAll('circle').data(data);
-  updateSelection.enter()
-      .append('circle')
-      .attr('r', 10)
-      .attr('cx', d => projection.latLngToLayerPoint(d).x)
-      .attr('cy', d => projection.latLngToLayerPoint(d).y)
-      .attr('fill', 'red');
+  const updateSelection = selection.selectAll('circle').data(data, d => d.id);
+
+  // this creates new circles if the data is updated
+  updateSelection.enter().append('circle')
+    .attr('stroke', 'black')
+    .attr('fill', 'red');
+  updateSelection.exit().remove();
+
+  // this updates existing circles when they need to be redrawn
+  // (if you need to react to data updates, this is the place)
+  updateSelection
+    .attr('r', 10 / projection.scale)
+    .attr('cx', d => projection.latLngToLayerPoint(d.latLng).x)
+    .attr('cy', d => projection.latLngToLayerPoint(d.latLng).y);
 });
 
 d3Overlay.addTo(map);
