b9a6ed5cfe7020aeab181eb84a0b06aeb928dafe
Stephen Matta <stephenjmatta+github@gmail.com>
2015-10-27 09:40:08 -0400

    Proof of concept; needs work

--- es6/main.js
+++ es6/main.js
@@ -1,9 +1,8 @@
-import L from 'imports?L=leaflet!exports?L!leaflet.markercluster';
-import faker from 'faker';
+import L from 'leaflet';
+import d3 from 'd3';
 import async from 'async';
+import faker from 'faker';
 import 'leaflet/dist/leaflet.css';
-import 'leaflet.markercluster/dist/MarkerCluster.css';
-import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import './main.css';
 
 const map = L.map('map').setView([38.89, -77.09], 4);
@@ -14,30 +13,20 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 
 L.Icon.Default.imagePath = 'http://api.tiles.mapbox.com/mapbox.js/v1.0.0beta0.0/images';
 
-const progress = document.getElementById('progress');
-const progressBar = document.getElementById('progress-bar');
-const updateProgressBar = (processed, total, elapsed) => {
-  if (elapsed > 1000) {
-    progress.style.display = 'block';
-    progressBar.style.width = Math.round(processed / total * 100) + '%';
-  }
-  if (processed === total) {
-    progress.style.display = 'none';
-  }
-};
+// TODO: don't assume 0, 0; use map.pixelOrigin()
+const mapSize = map.getSize();
+const svgContainer = d3.select(map.getPanes().overlayPane).append('svg')
+  .attr('width', mapSize.x)
+  .attr('height', mapSize.y);
+const g = svgContainer.append('g').attr('class', 'leaflet-zoom-hide');
 
-const markerCluster = L.markerClusterGroup({
-  animateAddingMarkers: true, // this can be disabled if it impacts performance
-  chunkedLoading: true,
-  chunkProgress: updateProgressBar,
-});
-
-map.addLayer(markerCluster);
-
-async.times(50000, (n, callback) => {
-  return async.nextTick(() => {
-    callback(null, L.marker([faker.address.latitude(), faker.address.longitude()]));
-  });
-}, (err, markers) => {
-  markerCluster.addLayers(markers);
+async.times(1000, (n, callback) => {
+  const latLng = [faker.address.latitude(), faker.address.longitude()];
+  const layerPoint = map.latLngToLayerPoint(latLng);
+  const circle = g.append('circle')
+    .attr('cx', layerPoint.x)
+    .attr('cy', layerPoint.y)
+    .attr('r', 10)
+    .attr('fill', 'red');
+  callback(null, circle);
 });
